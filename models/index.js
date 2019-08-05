397986054a5426c00a628c53a88d20b25608eb14
Matt Elliott <mattelliottus@gmail.com>
2019-08-04 18:22:35 -0700

    logging env var

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const config = require('../config/config');
 const Sequelize = require('sequelize');
+console.log('\n', process.env.use_heroku_db,'\n')
 if(process.env.use_heroku_db) {
   const sequelize = new Sequelize(
     process.env.JAWSDB_URL
